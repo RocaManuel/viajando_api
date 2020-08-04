729868346380a7f8742287067e51a2732fbc2691
ManuelIgnacioRoca <manuel.roca@id90travel.com>
2020-08-04 00:41:34 -0300

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,24 @@ export class App {
 
   public getApp() {
     this.setRoutes();
+    this.app.use(this.getCORS());
     return this.app;
+  }
+
+  private getCORS() {
+    return (req: any, res: express.Response, next: express.NextFunction) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+      // intercept OPTIONS method
+      if ('OPTIONS' === req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
+    }
   }
 
   private setRoutes() {
